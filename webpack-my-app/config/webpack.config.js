f97bb30579262a63b5f463b0b7527a0a7fbb94ec
Rudolf Olah <rudolf.olah.to@gmail.com>
2023-11-18 15:06:05 -0500

    Remove commented out code and update HeapSamplingPlugin import

--- webpack-my-app/config/webpack.config.js
+++ webpack-my-app/config/webpack.config.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const resolve = require('resolve');
-// const HeapSamplingPlugin = require("../../../heap-sampling-webpack-plugin/lib");
 const HeapSamplingPlugin = require("heap-sampling-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
